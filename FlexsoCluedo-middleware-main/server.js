3896deb64ec2f011432ea64a47c33030c720015a
Niels verschooten <niels.verschooten@student.ap.be>
2021-12-02 14:13:54 +0100

    requirements done

--- FlexsoCluedo-middleware-main/server.js
+++ FlexsoCluedo-middleware-main/server.js
@@ -191,11 +191,11 @@ function _writeGuess(currentAnswer, filePath){
       
       
     // Adding the new data to our object
-    myObject.push(currentAnswer)
+    myObject["guesses"].push(currentAnswer)
       
     // Writing to our JSON file
     var newData2 = JSON.stringify(myObject);
-    fs.writeFile("data2.json", newData2, (err) => {
+    fs.writeFile(filePath, newData2, (err) => {
       // Error checking
       if (err) throw err;
       console.log("New data added");
